dad4b7d55b3d92cb478262c931946a11d4370391
Ilya Lebedev <bacherko@gmail.com>
2019-04-03 20:49:20 +0300

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,20 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es6: true,
-    },
-    extends: ['airbnb-base', 'prettier'],
-    plugins: ['prettier'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-        ecmaVersion: 2018,
-    },
-    rules: {
-        'no-console': 0,
-        'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
-        'no-await-in-loop': 0,
-        'no-cond-assign': 0,
-        'prefer-destructuring': 0,
-        'require-yield': 0,
-        'func-names': 0,
-    },
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  globals: {},
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'no-console': 0,
+    'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+    'no-await-in-loop': 0,
+    'no-cond-assign': 0,
+    'prefer-destructuring': 0,
+    'func-names': 0,
+  },
 };
